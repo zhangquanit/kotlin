33feb1b85ba8c13f20894b66072689bc02127203
zhangquan <zhangquanit@126.com>
2020-11-08 23:46:04 +0800

    update

--- app/src/main/java/com/example/kotlin/kotlin/basic/BasicTest.kt
+++ app/src/main/java/com/example/kotlin/kotlin/basic/BasicTest.kt
@@ -10,23 +10,25 @@ class BasicTest {
     fun main(args: Array<String>) {
 
         //三目运算
-        var a=1
-        var b=2
-        if (a>b) a else b
+        var a = 1
+        var b = 2
+        if (a > b) a else b
 
         //表达式作为函数体
         fun sum(a: Int, b: Int) = a + b
-        sum(1,2)
+        sum(1, 2)
     }
 
     fun nullTest(str: String?) {
         /**
-            String b = str;
-            if (str == null) {
-               b = "default";
-            }
+        String b = str;
+        if (str == null) {
+        b = "default";
+        }
          */
         var b = str ?: "default";
         println("$b")
+
+        str ?: return  //if(str==null)return;
     }
 }
